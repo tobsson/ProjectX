ce61d9a93ba76569cd0a70daa9d9fea363ffdcc5
andriuskinas <andrius.murnikovas@gmail.com>
2015-11-12 13:04:54 +0100

    Added new activity SearchResuts and updated Main activity

--- FindaTweet/app/src/main/java/com/example/andrius/findatweet/MainActivity.java
+++ FindaTweet/app/src/main/java/com/example/andrius/findatweet/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -36,6 +37,17 @@ public class MainActivity extends AppCompatActivity {
             finish();
             return;
         }
+    }
+
+
+        public void onButtonClick(View v){
+        //open SearchResultsActivity when search button is clicked
+
+        if (v.getId() == R.id.btSearch){
+            Intent i = new Intent (MainActivity.this, SearchResultsActivity.class);
+            startActivity(i);
+        }
+    }
 
     }
-}
+
