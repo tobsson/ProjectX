32e41189e0fc84e7f2bb0cac08c0e520ba56be88
andriuskinas <andrius.murnikovas@gmail.com>
2015-12-16 12:16:08 +0100

    ListView without much functionality yet.

--- FindaTweet/app/src/main/java/com/example/andrius/findatweet/MainActivity.java
+++ FindaTweet/app/src/main/java/com/example/andrius/findatweet/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.andrius.findatweet;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
@@ -7,11 +9,18 @@ import android.database.sqlite.SQLiteCursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity  implements SearchView.OnQueryTextListener,
@@ -22,31 +31,65 @@ public class MainActivity extends AppCompatActivity  implements SearchView.OnQue
     public Button button;
     private SuggestionsDatabase database;
     private SearchView searchView;
-
-
-
-
-
-
-
     private SharedPreferences preferences;
 
+            HashMap<String, List<String>> countriesHashMap;
+            List<String> countriesHashMapKeys;
+            ExpandableListView expandableListView;
+            ListCustomAdapter adapter;
 
-    @Override
+
+
+
+            @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         database = new SuggestionsDatabase(this);
         searchView = (SearchView) findViewById(R.id.searchView1);
         searchView.setOnQueryTextListener(this);
         searchView.setOnSuggestionListener(this);
 
-        AutoCompleteTextView search_text = (AutoCompleteTextView) searchView.findViewById(searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null));
+
+
+
+
+
+                AutoCompleteTextView search_text = (AutoCompleteTextView) searchView.findViewById(searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null));
         search_text.setThreshold(1);
+///Code for expandableListView
+          expandableListView = (ExpandableListView) findViewById(R.id.expandableList);
+          countriesHashMap = CountryDataProvider.getDataHashMap();
+          countriesHashMapKeys = new ArrayList<String>(countriesHashMap.keySet());
 
+          adapter = new ListCustomAdapter(this, countriesHashMap, countriesHashMapKeys);
+          expandableListView.setAdapter(adapter);
 
+           expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+                    @Override
+                    public void onGroupExpand(int groupPosition) {
+                        Toast.makeText(MainActivity.this,
+                                countriesHashMapKeys.get(groupPosition)
+                                        + " expanded", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+           expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+                    @Override
+                    public void onGroupCollapse(int groupPosition) {
+                        Toast.makeText(MainActivity.this, countriesHashMapKeys.get(groupPosition) + " collapsed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+           expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+                    @Override
+                    public boolean onChildClick(ExpandableListView expandableListView, View clickedView, int groupPosition, int childPosition, long id) {
+                        Toast.makeText(MainActivity.this, "Selected " + countriesHashMap.get(countriesHashMapKeys.get(groupPosition)).get(childPosition)
+                                + " from " + countriesHashMapKeys.get(groupPosition), Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
 
 
 
@@ -70,15 +113,18 @@ public class MainActivity extends AppCompatActivity  implements SearchView.OnQue
         }
     }
 
+            @Override
+            public boolean onCreateOptionsMenu(Menu menu) {
+                MenuInflater inflater = getMenuInflater();
+                inflater.inflate(R.menu.menu_main, menu);
 
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
 
-            return true;
-        }
+
+                return super.onCreateOptionsMenu(menu);
+            }
+
+
 
         @Override
         public boolean onOptionsItemSelected(MenuItem item) {
@@ -86,17 +132,14 @@ public class MainActivity extends AppCompatActivity  implements SearchView.OnQue
             // automatically handle clicks on the Home/Up button, so long
             // as you specify a parent activity in AndroidManifest.xml.
 
-            int id = item.getItemId();
 
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
+           // int id = item.getItemId();
+           // if (id == R.id.action_location) {
+               // startActivity(new Intent(getApplicationContext(),SearchResultsActivity.class));
+                //return true;
+           // }
             return super.onOptionsItemSelected(item);
         }
-
-
-
 
             @Override
             public boolean onSuggestionSelect(int position) {
@@ -111,6 +154,9 @@ public class MainActivity extends AppCompatActivity  implements SearchView.OnQue
                 int indexColumnSuggestion = cursor.getColumnIndex( SuggestionsDatabase.FIELD_SUGGESTION);
 
                 searchView.setQuery(cursor.getString(indexColumnSuggestion), false);
+
+
+
 
                 return true;
             }
@@ -134,7 +180,7 @@ public class MainActivity extends AppCompatActivity  implements SearchView.OnQue
                             columns , columnTextId
                             , 0);
 
-                    searchView.setSuggestionsAdapter(simple);
+             searchView.setSuggestionsAdapter(simple);
                     return true;
                 }
                 else
@@ -142,12 +188,7 @@ public class MainActivity extends AppCompatActivity  implements SearchView.OnQue
                     return false;
                 }
             }
-
-
-
-
-
-        public void onButtonClick(View v){
+            public void onButtonClick(View v){
         //open SearchResultsActivity when search button is clicked
 
         if (v.getId() == R.id.btSearch){
